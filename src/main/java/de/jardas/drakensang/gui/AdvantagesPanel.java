c0bc11fabd34155a3306940ce7683c865fbd2db3
phjardas <phjardas>
2008-09-02 10:55:02 +0000

    Ein Charakter darf maximal vier Vor- oder Nachteile haben.

--- src/main/java/de/jardas/drakensang/gui/AdvantagesPanel.java
+++ src/main/java/de/jardas/drakensang/gui/AdvantagesPanel.java
@@ -9,6 +9,7 @@
  */
 package de.jardas.drakensang.gui;
 
+import de.jardas.drakensang.DrakensangException;
 import de.jardas.drakensang.dao.Messages;
 import de.jardas.drakensang.model.Advantage;
 import de.jardas.drakensang.model.Advantage.Effect;
@@ -17,6 +18,8 @@ import de.jardas.drakensang.model.Character;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import java.text.Collator;
 
@@ -24,23 +27,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 
 
 public class AdvantagesPanel extends JPanel {
     private Character character;
+    private final Set<JCheckBox> boxes = new HashSet<JCheckBox>();
 
     public AdvantagesPanel() {
         setLayout(new GridBagLayout());
     }
 
     private void update() {
+        boxes.clear();
         removeAll();
         addAdvantageFields();
     }
@@ -67,15 +72,23 @@ public class AdvantagesPanel extends JPanel {
     private void addAdvantageField(final Advantage advantage, int row) {
         final JCheckBox box = new JCheckBox();
         box.setSelected(getCharacter().getAdvantages().contains(advantage));
-        box.addChangeListener(new ChangeListener() {
-                public void stateChanged(ChangeEvent e) {
+        box.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
                     if (box.isSelected()) {
-                        getCharacter().getAdvantages().add(advantage);
+                        if (getCharacter().getAdvantages().size() >= 4) {
+                            throw new DrakensangException(
+                                "A character must not have more than four treats!");
+                        } else {
+                            getCharacter().getAdvantages().add(advantage);
+                        }
                     } else {
                         getCharacter().getAdvantages().remove(advantage);
                     }
+
+                    enableUncheckedBoxes(getCharacter().getAdvantages().size() < 4);
                 }
             });
+        boxes.add(box);
 
         add(box,
             new GridBagConstraints(0, row, 1, 1, 0, 0, GridBagConstraints.WEST,
@@ -101,6 +114,14 @@ public class AdvantagesPanel extends JPanel {
         add(new JLabel(effects.toString()),
             new GridBagConstraints(2, row, 1, 1, 0, 0, GridBagConstraints.WEST,
                 GridBagConstraints.NONE, new Insets(3, 6, 3, 6), 0, 0));
+    }
+
+    private void enableUncheckedBoxes(boolean enabled) {
+        for (JCheckBox box : boxes) {
+            if (!box.isSelected()) {
+                box.setEnabled(enabled);
+            }
+        }
     }
 
     private String getName(Advantage adv) {
