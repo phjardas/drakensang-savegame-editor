c42080882b1895fc3364315ac702e9bfef4d05ec
phjardas <phjardas>
2010-01-04 19:49:12 +0000

    *** empty log message ***

--- src/test/java/de/jardas/drakensang/dao/ArchetypeDaoTest.java
+++ src/test/java/de/jardas/drakensang/dao/ArchetypeDaoTest.java
@@ -1,5 +1,9 @@
 package de.jardas.drakensang.dao;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -11,6 +15,13 @@ public class ArchetypeDaoTest {
 
 	@Test
 	public void testLoadArchetypes() {
-		ArchetypeDao.get("archetype_MI_KR_W");
+		System.out.println("Hairs: "+ Arrays.toString(ArchetypeDao.getHairs()));
+		assertEquals("Number of hairs", 55, ArchetypeDao.getHairs().length);
+		
+		System.out.println("Faces: "+ Arrays.toString(ArchetypeDao.getFaces()));
+		assertEquals("Number of faces", 30, ArchetypeDao.getFaces().length);
+		
+		System.out.println("Bodies: "+ Arrays.toString(ArchetypeDao.getBodies()));
+		assertEquals("Number of bodies", 30, ArchetypeDao.getBodies().length);
 	}
 }
